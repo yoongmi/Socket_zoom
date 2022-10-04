9c01394c42f66a827d9884aaaf7f8addecbc1a25
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-10-04 22:41:42 +0900

    #1 websockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`); // socket 서버로 접속.
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form#message");
+const nickForm = document.querySelector("form#nickname");
+
+//socket event
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Connected to Server ❌");
+});
+
+// JSON파일을 string으로 변형.
+// (javascript object를 서버에 보내지 않고 string으로 변형하여 보냄.) : 서버에서 javascript를 안쓸 수도 있으니까.
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+//message
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+
+  input.value = "";
+}
+//nickname
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.setAttribute("placeholder", `Your nick name is ${input.value}`);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { SocketAddress } from "net";
 
 // express 어플리케이션 구성
 const app = express();
@@ -15,4 +18,32 @@ app.get("/*", (req, res) => res.redirect("/")); //catchall url (다른url 사용
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+// http,websocket 서버 두개 만듬.
+const server = http.createServer(app); //http 서버
+const wss = new WebSocket.Server({ server }); //websocket 서버
+
+const allSockets = [];
+
+// websocket 서버에 연결되었을 떄.
+wss.on("connection", (socket) => {
+  allSockets.push(socket);
+  allSockets["nickname"] = "anon"; // 닉네임 정하지 않았을때.
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Connected to Browser ❌"));
+
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString());
+    switch (message.type) {
+      case "new_message": //메세지 추가.
+        allSockets.forEach((aSocket) =>
+          aSocket.send(`${allSockets.nickname}:${message.payload}`)
+        );
+        break;
+      case "nickname": //닉네임 변경
+        allSockets["nickname"] = message.payload;
+        break;
+    }
+  });
+});
+
+server.listen(80, handleListen);
